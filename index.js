fcf64c47b5b5cdb8a558069668c20a07cc105253
awade <newuser@MacBook-3.local>
2017-09-28 19:33:37 -0600

    can click bubbles to change slide

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ adjCSS('imgHeight', window.innerHeight);
 queries.forEach(function(query) {
     bubbleRow.innerHTML += `<div class="bubble"></div>`;
 });
+
 queries.forEach(function(query) {
     carousel.innerHTML += `<div class="slide" style="background: #222 url('http://source.unsplash.com/random/`+ initMeasurements + '?' + query + `') no-repeat center; background-size: contain;"></div>`;
 });
@@ -18,27 +19,30 @@ document.querySelector('.slide').classList.add('active');
 document.querySelector('.bubble').classList.add('active');
 let slides = document.querySelectorAll('.slide');
 let bubbles = document.querySelectorAll('.bubble');
+[].forEach.call(bubbles, function(e){e.addEventListener('click', bubbleChange, false)});
 
+// shifts two css variables on resize
 window.addEventListener('resize', function() {
     adjCSS('imgWidth', window.innerWidth);
     adjCSS('imgHeight', window.innerHeight);
 });
 
+// make side arrow buttons work
 const rightControl = document.querySelector('.go-right'),
 leftControl = document.querySelector('.go-left');
-
 rightControl.addEventListener('click', function() { moveSlide('right', slides) });
 leftControl.addEventListener('click', function() { moveSlide('left', slides) });
 
+// allow navigation with arrow keys
 window.addEventListener('keydown', function(e) {
     if (e.keyCode == 39) moveSlide('right', slides);
     if (e.keyCode == 37) moveSlide('left', slides);
 })
 
+// initiate mobile UI
 document.addEventListener("touchstart", touch2Mouse, true);
 document.addEventListener("touchmove", touch2Mouse, true);
 document.addEventListener("touchend", touch2Mouse, true);
-
 document.addEventListener('mousedown', mousedownHandler, true);
 
 
@@ -108,4 +112,16 @@ function moveSlide(direction, target) {
 }
 function adjCSS(name, value) {
     document.documentElement.style.setProperty(`--${name}`, value + 'px');
+}
+
+function bubbleChange(e) {
+    let node = e.target;
+    document.querySelector('.bubble.active').classList.remove('active');
+    document.querySelector('.slide.active').classList.remove('active');
+
+    var i=0, e=node;
+    while (e = e.previousSibling) { console.log(i); ++i;}
+
+    node.classList.add('active');
+    slides[i].classList.add('active');
 }
